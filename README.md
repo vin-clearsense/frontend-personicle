38075850eaa1694012a53d005eb1797fda564917
vaibhap1 <vaibhap1@uci.edu>
2021-10-12 17:44:44 -0700

    First Commit
    Included OAuth using nextJS for Github; working on gmail and email login

---
+++ README.md
@@ -0,0 +1,1 @@
+# Personicle Frontend Application
---
+++ frontend_personicle/pages/data-connections.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/client";
+
+export default function DataConnection(){
+    const[session, loading] = useSession();
+    const[content, setContent] = useState();
+
+    useEffect(() => {
+        const fetchData = async() => {
+            const res = await fetch("/api/data-connections");
+            const json = await res.json();
+
+            if(json.content){
+                setContent(json.content)
+            }
+        }
+        fetchData();
+    }, [session]);
+
+    if( typeof window !== "undefined" && loading) return null;
+
+    if (!session){
+        return (
+            <main>
+                <div>
+                    <h1> You aren't logged in. Please log in</h1>
+                </div>
+            </main>
+        )
+    }
+
+    return(
+        <main>
+            <div>
+                <h1>Protected Page</h1>
+                <p>
+                    {content}
+                </p>
+            </div>
+        </main>
+    )
+
+}
